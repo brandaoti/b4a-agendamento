5c75a3b3ec355bfa860d7243bdd21204803e137d
Daniel Brandao <daniel.brandao.dev@gmail.com>
2024-08-07 11:33:44 -0300

    feat:(added get professional and format professional/specialty)

---
+++ cloud/professional/get-professional.js
@@ -0,0 +1,38 @@
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+
+
+
+const { formatProfessional } = require("./format-professional");
+
+const Professional = Parse.Object.extend("Professional");
+const Specialty = Parse.Object.extend("Specialty");
+
+Parse.Cloud.define("v1-get-professionals", async (request) => {
+	const params = request.params;
+
+	const query = new Parse.Query(Professional);
+
+	query.include("specialties", "insurances", "services");
+
+	if (request.params.specialtyId) {
+		const specialty = new Specialty();
+		specialty.id = request.params.specialtyId;
+		query.equalTo("specialties", specialty);
+	}
+
+	if (request.params.lat && request.params.long) {
+		const point = new Parse.GeoPoint({ latitude: params.lat, longitude: params.long });
+		query.withinKilometers("location", point, request.params.maxDistance || 50);
+	}
+
+	if (request.params.limit && request.params.skip) {
+		query.limit(params.limit);
+		query.skip(params.skip);
+	}
+
+	const results = await query.find({ useMasterKey: true });
+
+	return results.map((data) => formatProfessional(data.toJSON()));
+}, {
+	fields: {}
+});
---
+++ cloud/specialty/format-specialty.js
@@ -0,0 +1,11 @@
+
+
+// Formatted 
+function formatSpeciality(s) {
+    return {
+        id: s.objectId,
+        name: s.name,
+    }
+}
+
+module.exports = { formatSpeciality }
